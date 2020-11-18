7eb202e183793379899344675a2161a07b69ce27
Brian <blsvsanteliz@gmail.com>
2020-11-18 18:53:15 -0400

    Finish register

--- src/controllers/registroController.js
+++ src/controllers/registroController.js
@@ -6,18 +6,30 @@ class RegistroController {
   }
   async postRegistro(req, res) {
     const { usuario, clave, repetir } = req.body;
-    console.log(typeof usuario);
     if (!usuario || !clave || !repetir) {
       res.render("registrarse/agregar", {
         error: "Los datos solicitados son necesarios",
       });
       return;
     }
-    if (clave !== repetir) {
+    if (clave.length <= 5) {
       res.render("registrarse/agregar", {
-        error: "Las contraseñas no coinciden",
+        error: "Las contraseñas deben tener minimo 6 caracteres",
+        usuario,
+        clave,
+        repetir,
       });
       return;
+    } else {
+      if (clave !== repetir) {
+        res.render("registrarse/agregar", {
+          error: "Las contraseñas no coinciden",
+          usuario,
+          clave,
+          repetir,
+        });
+        return;
+      }
     }
     const usuarioExistente = await pool.query(
       "SELECT * FROM usuarios WHERE usuario = ?",
@@ -31,7 +43,6 @@ class RegistroController {
     }
     const salt = await bcrypt.genSalt(10);
     const nuevaClave = await bcrypt.hash(clave, salt);
-    //TODO no registrar mas de un usuario
     try {
       await pool.query(
         `insert into usuarios
